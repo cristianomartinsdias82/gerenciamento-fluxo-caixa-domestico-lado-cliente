bcec58e087df3e998eef233e6680aa8075b44732
Cristiano Martins Dias <crisdi@gmail.com>
2026-01-19 12:24:43 -0300

    Initial - project creation + App component barebone

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+
+      </header>
+      <main>
+        <nav>
+        
+        </nav>
+        <section>
+
+        </section>
+      </main>
+      <footer>
+
+      </footer>      
+    </>
+  )
+}
+
+export default App
